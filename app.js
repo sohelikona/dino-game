67b510e04cb9abae8bbd800c4326355193970ff1
Soheli Arefin Kona <kritiakter0@gmail.com>
2023-01-30 10:30:26 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const character = document.querySelector('#character')
+const block = document.querySelector('#block')
+
+function jump() {
+    if (character.classList != 'animate') {
+        character.classList.add('animate');
+    }
+   
+    setTimeout(function() {
+        character.classList.remove('animate')
+    }, 500);
+}
+
+
+let checkDead = setInterval(function() {
+   let characterTop =  parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+   let blockLeft =  parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+    if (blockLeft < 20 && blockLeft > 0 &&  characterTop >= 130) {
+        block.style.animation = 'none'
+        block.style.display = 'none'
+        alert('You lose!')
+    }
+}, 10)
+
+
+
+
